ce0ef75431b84bb4b118d79923fd0cd3bdf738d0
Daniel Flanagan <daniel@lyte.dev>
2021-08-10 17:48:54 -0500

    Add Deno deploy types

--- mod.ts
+++ mod.ts
@@ -1,3 +1,6 @@
+// `deployctl types > deployctl.d.ts`
+/// <reference path="./deployctl.d.ts" />
+
 addEventListener("fetch", (ev) => {
   ev.respondWith(
     new Response("Hello, deno.lyte.dev visitor!", {
