20d484c1a88897652b6b9da77b6087f3a9c45cae
Daniel Flanagan <daniel@lyte.dev>
2021-08-11 13:22:42 -0500

    Fix types being imported

--- mod.ts
+++ mod.ts
@@ -1,14 +1,28 @@
 // `deployctl types > deployctl.d.ts`
-/// <reference path="./deployctl.d.ts" />
 
-addEventListener("fetch", (ev) => {
-  ev.respondWith(
-    new Response("Hello, deno.lyte.dev visitor!", {
-      status: 200,
+addEventListener("fetch", (ev) => ev.respondWith(handleRequest(ev.request)))
+
+const handleRequest = async (req: Request): Promise<Response> => {
+  const method = req.method.toLowerCase().trim()
+  return new Response(
+    JSON.stringify(error("not_found")),
+    {
+      status: 404,
+      statusText: 'Not Found',
       headers: {
-        server: "deploy",
-        "content-type": "text/plain",
+        'content-type': 'application/json; charset=utf-8',
       },
-    }),
-  );
-});
+    }
+  )
+}
+
+interface AppError {
+  error: {
+    message: any,
+    _for_humans?: any,
+  }
+}
+
+const error = (message: any, human_friendly_message?: any): AppError => ({error: {message, _for_humans: human_friendly_message || message}})
+
+export default {}
